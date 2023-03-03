e7a2266b63d1aaf7d10e142a6a8aea6549a27ad4
817677 <817677@605-110-S02>
2023-03-03 13:42:42 -0700

    Add project files.

---
+++ dinojumper/dinojumper.py
@@ -0,0 +1,116 @@
+import pygame
+import random
+import time  
+print("hello world")
+score = 0
+pygame.init()
+screen = pygame.display.set_mode((640,480))
+pygame.display.set_caption("Dino Jumper")
+doExit=False
+clock = pygame.time.Clock()
+#game variables
+p1x = 20
+p1y = 450
+touchGround = False
+hasDoubleJump = True
+doubleJumpTimer = 0
+yVel = 0
+CactusHeights= [43,43,43,43,43]
+CactusXpos=[]
+cactuswait = 0
+CactusSpeed = 5
+for x in range(1,5):
+    CactusXpos.append(random.randrange(200,3000))
+CactusImg = pygame.image.load('cactus.png')
+#game loop
+while not doExit:
+    #timer section
+    clock.tick(60)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            doExit=True;
+    p1y += yVel
+    cactuswait += 1
+    #replace 5 with cactus speed
+    if cactuswait > 100:
+        CactusSpeed += 0.02
+        CactusXpos = [x - CactusSpeed for x in CactusXpos]
+        for x in range(len(CactusXpos)):
+            if CactusXpos[x]<0:
+                CactusXpos[x]=random.randrange(640,5000)
+                print("reset to", CactusXpos[x])
+        for x, y in zip(CactusXpos, CactusHeights):
+            a = pygame.Rect((x, 480-y), (30,80))
+            b = pygame.Rect((p1x, p1y), (30, 30))
+            if a.colliderect(b) == True:
+                print("COLLISION")
+                print("Final Score", score)
+                time.sleep(5)
+                doExit=True
+    if (p1y+30) == 480:
+        touchGround = True
+        hasDoubleJump = True
+    else:
+        touchGround = False
+    #gravity
+    if touchGround == False:
+        yVel += 0.5
+    else:
+        yVel = 0
+    #input section
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_SPACE] and touchGround == True:
+        yVel=-10
+        for i in range(10):
+            doubleJumpTimer += 10
+    #if keys[pygame.K_w] and touchGround == False and hasDoubleJump == True and doubleJumpTimer == 100:
+        #yVel=-5
+        #doubleJumpTimer = 0
+        #hasDoubleJump = False
+    screenr = 200
+    screeng = 200
+    screenb = 210
+    screen.fill((screenr,screeng,screenb))
+    playerr = 50
+    playerg = 200
+    playerb = 0
+    pygame.draw.rect(screen, (playerr, playerg, playerb), (p1x, p1y, 30, 30), 0)
+    for x, y in zip(CactusXpos, CactusHeights):
+        screen.blit(CactusImg, (x-15, 480-y))
+    score = score+1
+    print("score = ", score)
+    
+    pygame.display.flip()
+pygame.quit()
+#⠀⠀⠘⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡜⠀⠀⠀
+#⠀⠀⠀⠑⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡔⠁⠀⠀⠀
+#⠀⠀⠀⠀⠈⠢⢄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⠴⠊⠀⠀⠀⠀⠀
+#⠀⠀⠀⠀⠀⠀⠀⢸⠀⠀⠀⢀⣀⣀⣀⣀⣀⡀⠤⠄⠒⠈⠀⠀⠀⠀⠀⠀⠀⠀
+#⠀⠀⠀⠀⠀⠀⠀⠘⣀⠄⠊⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+#⠀
+#⣿⣿⣿⣿⣿⣿⣿⣿⡿⠿⠛⠛⠛⠋⠉⠈⠉⠉⠉⠉⠛⠻⢿⣿⣿⣿⣿⣿⣿⣿
+#⣿⣿⣿⣿⣿⡿⠋⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠛⢿⣿⣿⣿⣿
+#⣿⣿⣿⣿⡏⣀⠀⠀⠀⠀⠀⠀⠀⣀⣤⣤⣤⣄⡀⠀⠀⠀⠀⠀⠀⠀⠙⢿⣿⣿
+#⣿⣿⣿⢏⣴⣿⣷⠀⠀⠀⠀⠀⢾⣿⣿⣿⣿⣿⣿⡆⠀⠀⠀⠀⠀⠀⠀⠈⣿⣿
+#⣿⣿⣟⣾⣿⡟⠁⠀⠀⠀⠀⠀⢀⣾⣿⣿⣿⣿⣿⣷⢢⠀⠀⠀⠀⠀⠀⠀⢸⣿
+#⣿⣿⣿⣿⣟⠀⡴⠄⠀⠀⠀⠀⠀⠀⠙⠻⣿⣿⣿⣿⣷⣄⠀⠀⠀⠀⠀⠀⠀⣿
+#⣿⣿⣿⠟⠻⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠶⢴⣿⣿⣿⣿⣿⣧⠀⠀⠀⠀⠀⠀⣿
+#⣿⣁⡀⠀⠀⢰⢠⣦⠀⠀⠀⠀⠀⠀⠀⠀⢀⣼⣿⣿⣿⣿⣿⡄⠀⣴⣶⣿⡄⣿
+#⣿⡋⠀⠀⠀⠎⢸⣿⡆⠀⠀⠀⠀⠀⠀⣴⣿⣿⣿⣿⣿⣿⣿⠗⢘⣿⣟⠛⠿⣼
+#⣿⣿⠋⢀⡌⢰⣿⡿⢿⡀⠀⠀⠀⠀⠀⠙⠿⣿⣿⣿⣿⣿⡇⠀⢸⣿⣿⣧⢀⣼
+#⣿⣿⣷⢻⠄⠘⠛⠋⠛⠃⠀⠀⠀⠀⠀⢿⣧⠈⠉⠙⠛⠋⠀⠀⠀⣿⣿⣿⣿⣿
+#⣿⣿⣧⠀⠈⢸⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠟⠀⠀⠀⠀⢀⢃⠀⠀⢸⣿⣿⣿⣿
+#⣿⣿⡿⠀⠴⢗⣠⣤⣴⡶⠶⠖⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⡸⠀⣿⣿⣿⣿
+#⣿⣿⣿⡀⢠⣾⣿⠏⠀⠠⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠛⠉⠀⣿⣿⣿⣿
+#⣿⣿⣿⣧⠈⢹⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣰⣿⣿⣿⣿
+#⣿⣿⣿⣿⡄⠈⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣠⣴⣾⣿⣿⣿⣿⣿
+#⣿⣿⣿⣿⣧⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣠⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿
+#⣿⣿⣿⣿⣷⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣴⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+#⣿⣿⣿⣿⣿⣦⣄⣀⣀⣀⣀⠀⠀⠀⠀⠘⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+#⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⡄⠀⠀⠀⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+#⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣧⠀⠀⠀⠙⣿⣿⡟⢻⣿⣿⣿⣿⣿⣿⣿⣿⣿
+#⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠇⠀⠁⠀⠀⠹⣿⠃⠀⣿⣿⣿⣿⣿⣿⣿⣿⣿
+#⣿⣿⣿⣿⣿⣿⣿⣿⡿⠛⣿⣿⠀⠀⠀⠀⠀⠀⠀⠀⢐⣿⣿⣿⣿⣿⣿⣿⣿⣿
+#⣿⣿⣿⣿⠿⠛⠉⠉⠁⠀⢻⣿⡇⠀⠀⠀⠀⠀⠀⢀⠈⣿⣿⡿⠉⠛⠛⠛⠉⠉
+#⣿⡿⠋⠁⠀⠀⢀⣀⣠⡴⣸⣿⣇⡄⠀⠀⠀⠀⢀⡿⠄⠙⠛⠀⣀⣠⣤⣤⠄
+#Copied!
